7d6cdcef8ca6efc28e3525f5a0f52eaf018dc07e
StringAna <tewaryantara21@gmail.com>
2024-10-08 11:28:52 -0400

    Removed Scroll Down

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -15,13 +15,13 @@ const Home = () => {
                 {/* <dotlottie-player src="https://lottie.host/7f0bf641-c55f-40b3-89ce-f61cb65c902c/dQrwp1xlWE.json" background="transparent" speed="1" style={{width: '300px', height: '300px'}} loop autoplay></dotlottie-player> */}
                 <dotlottie-player src="https://lottie.host/2e851f75-3bf2-43cf-958f-21317f76cd1c/Th4wqHOQpS.json" background="transparent" speed="1" style={{width: '300px', height: '300px'}} loop autoplay></dotlottie-player>
                 <h1 className="home__name">Antara Tewary</h1>
-                <span className="home__education">I'm a Machine Learning and Data Science Enthusiast</span>
+                <span className="home__education">Grace Hopper 2024 | Visionary Co-Lead at Google Developer Group at George Mason University | Unparalleled ability to  transform complex data into actionable insights | Masters Student at George Mason University | Specializes in Automation, Natural Language Processing, Data Mining, AI and ML | May 2025 Graduation</span>
 
                 <HeaderSocials />
 
                 <a href="#contact" className="btn"> Contact Me</a>
 
-                <ScrollDown />
+                {/* <ScrollDown /> */}
             </div>
 
             <Shapes />
