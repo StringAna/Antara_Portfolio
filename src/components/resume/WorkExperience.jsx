e862f5e6052b25cccf2a23227d961c1150b09def
StringAna <tewaryantara21@gmail.com>
2024-10-08 11:30:26 -0400

    Added updated information from master resume

--- src/components/resume/WorkExperience.jsx
+++ src/components/resume/WorkExperience.jsx
@@ -19,9 +19,99 @@
 const WorkExperience = [
   {
     id: 1,
-    title: 'Program Office Intern',
+    title:'Co-Lead, Google Developer Group at George Mason University',
+    company: 'Google Developer Group',
+    yearsActive: 'September 2024 – Present',
+    location: 'Fairfax, United States',
+    information: [
+      {
+        title:'Community Building and Engagement',
+        details:[
+          'A lack of a cohesive mission and strategy for the Google Developer Group at George Mason.',
+          'Develop the club\'s mission, goals, and strategic plan in collaboration with key stakeholders, which included Career Services Department, other technical clubs with a strong presence in their community.',
+          'Co-led the GDG and worked closely with faculty advisors and Google program managers, utilizing strategic planning and leadership skills.',
+          'Established a clear direction and purpose for the GDG, setting the foundation for future growth and success.',
+        ],
+      },
+      {
+        title:'Recruitment and Planning',
+        details:[
+          'The GDG needed a dedicated leadership team to manage various functions and ensure alignment with the club\'s vision',
+          'Recruit and onboard a leadership team, including officers for key roles, and provide training to align them with the vision and responsibilities.',
+          'Leveraged team building and organizational development skills to identify and recruit talented individuals, and conducted training sessions to ensure a shared understanding of the GDG\'s goals.',
+          'Assembled a skilled and aligned leadership team, ready to drive the initiatives forward.',
+        ],
+      },
+      {
+        title:'Promotion',
+        details:[
+          'The group needed to increase its visibility and attract members from diverse backgrounds and academic disciplines.',
+          'Promote the club to students across majors and class years by partnering with key university departments and delivering presentations.',
+          'Utilized partnerships, business development, and public speaking skills to collaborate with Career Services, Alumni Association, and the School of Engineering, and delivered engaging presentations at 15+ classes and student org meetings.',
+          'Significantly increased the GDG\'s reach and attracted a diverse membership base.',
+        ],
+      },
+      {
+        title:'Weekly learning challenge',
+        details:[
+          'The GDG sought to create ongoing opportunities for members to practice their coding skills and learn from each other.',
+          'Plan and execute a series of events, including tech talks, workshops, and hackathons, to engage members and provide value.',
+          'Leveraged project management and event planning skills to organize and execute 10+ events, including a successful hackathon and a workshop on cloud computing.',
+          'Delivered high-quality events that catered to the diverse interests of the club members and attracted new participants.',
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    title:'Graduate Teaching Assistant',
+    company: 'George Mason University, College of Engineering and Computing',
+    yearsActive: 'August 2024 – Present',
+    location: 'Fairfax, United States',
+    information:[
+      {
+        title:'Course Development and Delivery',
+        details:[
+          ' Graduate-level systems programming course needed engaging and accessible curriculum.',
+          'Develop and deliver innovative, hands-on curriculum for complex topics.',
+          'Introduced creative teaching methods focusing on practical learning and advanced data structures.',
+          'Increased student engagement by 20% and improved comprehension of low-level programming concepts.',
+        ],
+      },
+      {
+        title:'Student Support and Mentorship',
+        details:[
+          'Diverse graduate students faced challenges in understanding complex systems programming topics.',
+          'Foster inclusive learning environment and bridge gap between theory and practical application.',
+          'Provided clear guidance and mentored students through hands-on projects and collaborative learning.',
+          'Increased student confidence in systems development and enhanced preparation for industry roles.',
+        ],
+      },
+      {
+        title:'Grading and Assessment',
+        details:[
+          'Manual grading of assignments and exams was time-consuming and prone to errors.',
+          'Developed automated grading scripts to streamline the grading process.',
+          'Collaborated with faculty to ensure grading accuracy and consistency.',
+          'Reduced grading time by 50% and improved grading accuracy by 15%.',
+        ],
+      },
+      {
+        title:'Enhanced Communication and Collaboration',
+        details:[
+          'Needed to ensure clear communication with students and faculty.',
+          'Facilitated regular communication with students through announcements and office hours.',
+          'Collaborated with faculty to align on course objectives and assessments.',
+          'Improved student satisfaction by 30% and fostered a collaborative learning environment.',
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Business Automation Intern',
     company: 'George Mason University Fiscal Services',
-    yearsActive: 'October 2023 – Present',
+    yearsActive: 'October 2023 – September 2024',
     location: 'Fairfax, United States',
     information: [
       {
@@ -67,6 +157,52 @@ const WorkExperience = [
           'Served as the liaison between technical teams and business units, translating business needs into technical requirements.',
           'Applied project management best practices to ensure projects were delivered on time and within budget.',
           'Successfully managed multiple projects simultaneously, achieving a 15% reduction in project timelines and increasing overall team productivity.',
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: 'Software Consultant',
+    company: 'Mercedes Benz Research and Development India',
+    yearsActive: 'October 2020 - July 2023',
+    location: 'Bangalore, India',
+    information: [
+      
+      {
+        title: 'Digital Drawing',
+        details: [
+          'Digital Drawing project required innovative algorithms to optimize search and zoom functionality for high-dimensional TIF files (17ft x 17ft), reducing production time and enhancing user experience.',
+          'Implement advanced algorithms and techniques to efficiently handle and manipulate large-scale TIF images within the Digital Drawing project.',
+          'Developed and deployed cutting-edge algorithms leveraging the ImageMagick library to enable fast search and smooth zooming for TIF files. Implemented techniques such as tiling, pyramid structures, and progressive loading to efficiently manage and render high-resolution images. Utilized parallel processing and caching mechanisms to optimize performance and reduce memory footprint.',
+          'Reduced production time by 40% through efficient handling and manipulation of large-scale TIF files. Delivered 8-10 user-centric features, including seamless search and zoom functionality for high-dimensional images (17ft x 17ft), significantly enhancing the overall user experience and productivity',
+        ],
+      },
+      {
+        title: 'User Engagement Data Analysis',
+        details: [
+          'User engagement data lacked accurate analysis, limiting insights into usage patterns and improvements.',
+          'Analyze user engagement data using regression and clustering models to identify insights.',
+          'Applied regression and clustering techniques to user engagement data.',
+          'Increased accuracy by 20% and improved customer satisfaction.',
+        ],
+      },
+      {
+        title: 'User Statistics Data Issues',
+        details: [
+          'User statistics and reported issues data required preprocessing to enhance reliability.',
+          'Manipulate and preprocess data to improve reliability.',
+          'Conducted rigorous data manipulation and preprocessing techniques.',
+          'Improved data reliability by 15% and increased feature enhancement efficiency by 25%.',
+        ],
+      },
+      {
+        title: 'Cross-Functional Collaboration',
+        details: [
+          'Complex projects required collaboration with multiple teams and stakeholders.',
+          'Facilitated regular meetings with stakeholders to gather requirements and provide project updates.',
+          'Worked closely with quality assurance teams to ensure software quality and compliance with industry standards.',
+          'Improved project delivery timelines by 20% and enhanced overall team communication and collaboration.',
         ],
       },
     ],
